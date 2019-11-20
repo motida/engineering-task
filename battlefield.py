fbb05f4a382dfb0f757af71afd8087d2729b1df4
motida2 <motida2@gmail.com>
2019-11-21 01:28:00 +0200

    second commit

--- battlefield.py
+++ battlefield.py
@@ -6,14 +6,35 @@ BATTLESHIPS = {'S': (1, 1), 'D': (2, 1), 'C': (3, 1), 'A': (4, 1)}  # Values are
 
 
 class Battlefield:
+    """
+    battlefield class
+    After initialization holds the following data structures:
+    1) Precomputed possible pieces on board where every piece is the set of positions making up the piece positions
+      on the board. Holds for each piece type (submarine etc.) a set of pieces that are not on the board and ready to be
+      picked at random. once picked removed along with all overlapping pieces.
+    2) A matrix holding the board occupied positions, each position either empty or holds part of a piece indicated
+       by the piece symbol (submarine -> S etc.).
+    3) Precomputed mapping between a piece and a 'buffered' piece that includes a spacing of one position in all
+       directions.  Cached for performance and used in overlapping calculation.
+
+    TODO: some optimization for overlap calculation
+          pre shuffling of free pieces which will effectively randomize in advance
+          parameterize script with argparse providing parameters like board size , number of initial pieces etc.
+    """
     def __init__(self, n, m):
+        """
+        Construct battlefield and all data structures
+        :param n: number rows of battleship
+        :param m: number of columns of battleship
+        """
         self.m = m
         self.n = n
         self.board = [row[:] for row in [[None] * m] * n]
 
-        self.free_battleship_pieces = self._generate_battleship_locations()
+        self.free_battleship_pieces = self.__generate_battleship_locations()
+        self.buffered_battleship_pieces = self.__generate_battleship_with_buffer()
 
-    def _generate_battleship_locations(self):
+    def __generate_battleship_locations(self):
         """
         Generate all potential pieces, each piece is represented by a set of position on battlefield
         :return: set of free pieces
@@ -21,13 +42,13 @@ class Battlefield:
         free_pieces = {}
         for battleship_type in BATTLESHIPS:
             dimension = BATTLESHIPS[battleship_type]
-            free_pieces[battleship_type] = self._get_battleship_type_pieces(dimension)
+            free_pieces[battleship_type] = self.__get_battleship_type_pieces(dimension)
             if dimension[0] != dimension[1]:  # switch orientation if necessary
                 free_pieces[battleship_type] = free_pieces[battleship_type] | \
-                                                  self._get_battleship_type_pieces((dimension[1], dimension[0]))
+                                                  self.__get_battleship_type_pieces((dimension[1], dimension[0]))
         return free_pieces
 
-    def _get_battleship_type_pieces(self, dimension):
+    def __get_battleship_type_pieces(self, dimension):
         """
         Generate all possible pieces for dimension
         :param dimension: represent a piece with specific orientation
@@ -40,10 +61,31 @@ class Battlefield:
                 for row in range(self.n - x_size + 1)
                 for col in range(self.m - y_size + 1)}
 
+    def __generate_buffered_piece(self, piece):
+        """
+        Generate a piece that include a buffer surrounding original piece
+        :param piece:
+        :return: buffered_piece
+        """
+        buffered_piece = set()
+        for pos in piece:
+            buffered_pos = {(pos[0] + x_delta, pos[1] + y_delta) for x_delta in (-1, 0, 1) for y_delta in (-1, 0, 1)
+                            if 0 <= (pos[0] + x_delta) < self.n and 0 <= (pos[1] + y_delta) < self.m}
+
+            buffered_piece.update(buffered_pos)
+
+        return buffered_piece
+
+    def __generate_battleship_with_buffer(self):
+        buffered_battleship_pieces = {piece: self.__generate_buffered_piece(piece)
+                                      for battleship_type in self.free_battleship_pieces
+                                      for piece in self.free_battleship_pieces[battleship_type]}
+        return buffered_battleship_pieces
+
     def __str__(self):
         """
         Generate a string with battlefield representation 
-        :return: 
+        :return: board as string
         """
         output = ''
         for row in range(self.n):
@@ -52,7 +94,7 @@ class Battlefield:
             output += '\n'
         return output
 
-    def _place_in_board(self, piece, piece_symbol):
+    def __place_in_board(self, piece, piece_symbol):
         """
         Places piece on board for easy printing 
         :param piece: 
@@ -62,31 +104,18 @@ class Battlefield:
         for (row, col) in piece:
             self.board[row][col] = piece_symbol
 
-    def _remove_overlapping_free_locations(self, piece):
+    def __remove_overlapping_free_locations(self, piece):
         """
         This will remove the placed piece itself and all other pieces whose locations overlaps with placed piece
         from free pieces pool
         """
-        buffered_piece = self._generate_buffered_piece(piece)
+        buffered_piece = self.buffered_battleship_pieces[piece]
         for battleship_type in BATTLESHIPS:
             locations_to_discard = []
             for free_piece in self.free_battleship_pieces[battleship_type]:
                 if free_piece & buffered_piece:  # points in common means they overlap
                     locations_to_discard.append(free_piece)
             self.free_battleship_pieces[battleship_type].difference_update(locations_to_discard)
-
-    def _generate_buffered_piece(self, piece):
-        """
-        Generate a piece that include a buffer surrounding original piece
-        :param piece: 
-        :return: buffered_piece
-        """
-        buffered_piece = set()
-        for pos in piece:
-            buffered_pos = {(pos[0] + x_delta, pos[1] + y_delta) for x_delta in (-1, 0, 1) for y_delta in (-1, 0, 1)}
-            buffered_piece.update(buffered_pos)
-
-        return buffered_piece
 
     def place_battleship(self, battleship_type):
         """
@@ -98,8 +127,8 @@ class Battlefield:
         if len(self.free_battleship_pieces[battleship_type]) == 0:
             return None
         piece = random.choice(tuple(self.free_battleship_pieces[battleship_type]))
-        self._place_in_board(piece, battleship_type)
-        self._remove_overlapping_free_locations(piece)
+        self.__place_in_board(piece, battleship_type)
+        self.__remove_overlapping_free_locations(piece)
         return piece
 
     def place_submarine(self):
@@ -137,10 +166,10 @@ if __name__ == '__main__':
         if battleship_type == 'S':
             battlefield.place_submarine()
         elif battleship_type == 'D':
-            battlefield.place_submarine()
+            battlefield.place_destroyer()
         elif battleship_type == 'C':
             battlefield.place_cruiser()
-        else:
+        else:  # 'A'
             battlefield.place_carrier()
         # Alternatively can just do
         # battlefield.place_battleship(battleship_type)
